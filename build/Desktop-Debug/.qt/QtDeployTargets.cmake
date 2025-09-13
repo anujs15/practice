6e9cbef5305759db26ed0d20f8aec983a8ba3cc6
harsha <uguy008@outlook.com>
2025-09-14 00:05:05 +0530

    chore: add .gitignore to ignore build/ and CMakeLists.txt.user; untrack existing files

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_apppractice_FILE /home/anujs/practice/build/Desktop-Debug/apppractice)
-set(__QT_DEPLOY_TARGET_apppractice_TYPE EXECUTABLE)
--- build/Desktop-Debug/.qt/apppractice_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "apppractice")
-set(working_dir "/home/anujs/practice")
-set(src_and_dest_list
-    "/home/anujs/practice/Resource.qrc"
-    "/home/anujs/practice/build/Desktop-Debug/practice/Resource.qrc"
-
-)
-set(timestamp_file "/home/anujs/practice/build/Desktop-Debug/.qt/apppractice_res.txt")
